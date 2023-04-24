a35d6d1b38fe83df686b4f92dc97e3a684839b9c
David Falcon Ayala <leobrana@gmail.com>
2023-04-24 07:56:26 -0700

    Update script.js

--- week_1/script.js
+++ week_1/script.js
@@ -5,3 +5,5 @@ let firstName = "David";
 let lastName = "Ayala";
 let yearsOfStudy = "2022-2024";
 let goal = "XR-Development";
+
+document.body.innerHTML = "<h1>My name is " + firstName + lastName + ". I have been at SCC for " + yearsOfStudy + " and plan to " + goal + " after completing my courses." 
