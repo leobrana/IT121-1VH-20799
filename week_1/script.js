60c755e871a63916647712fc36296f56d8b034ab
David Falcon Ayala <leobrana@gmail.com>
2023-04-24 08:14:26 -0700

    Update script.js

--- week_1/script.js
+++ week_1/script.js
@@ -1,9 +1,9 @@
 let d = new Date();
-document.body.innerHTML = "<h1>Today's date is " + d + "</h1>"
+document.body.innerHTML = "<h1>Today's date is " + d + "</h1>";
 
 let firstName = "David";
 let lastName = "Ayala";
 let yearsOfStudy = "2022-2024";
 let goal = "XR-Development";
 
-document.body.innerHTML = "<h1>My name is " + firstName + lastName + ". I have been at SCC for " + yearsOfStudy + " and plan to " + goal + " after completing my courses." 
+document.body.innerHTML = "<h1>My name is " + firstName + lastName + ". I have been at SCC for " + yearsOfStudy + " and plan to " + goal + " after completing my courses."; 
