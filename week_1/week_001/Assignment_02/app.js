5ba2dc8ee105ba3ad864534f9fc41659dd79fb16
David Falcon Ayala <leobrana@gmail.com>
2023-04-25 07:48:35 -0700

    Add files via upload

---
+++ week_1/week_001/Assignment_02/app.js
@@ -0,0 +1,10 @@
+let d = new Date();
+document.body.innerHTML = "<h1>Today's date is " + d + "</h1>";
+
+let firstName = "David";
+let lastName = "Ayala";
+let yearsOfStudy = "2022-2024";
+let goal = "XR-Development";
+
+document.body.innerHTML = "<h1>My name is " + firstName + lastName + ". I have been at SCC for " + yearsOfStudy + " and plan to " + goal + " after completing my courses."; 
+// JavaScript Document
